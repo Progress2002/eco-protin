ed2750b98109b30fb4f690eec38ecc34e58eb819
progress2002 <ezeamakachisom2002@gmail.com>
2025-07-08 02:51:26 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Eco Protin Website
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="text-lg">
+        This is a simple React app with Chakra UI and Tailwind CSS.
+      </p>
+    </div>
+  );
+}
+
+export default App;
